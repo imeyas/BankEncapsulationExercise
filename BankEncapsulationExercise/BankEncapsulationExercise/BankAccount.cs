63e6d472e9483cda9083471e18b7213f4cd24433
Ebo Onwunali <ebo@njgeneral.com>
2022-01-21 14:17:57 -0500

    Finished code for BankAccount Encapsulation Exercise lecture

---
+++ BankEncapsulationExercise/BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+
+        public BankAccount()
+        {
+
+        }
+        public BankAccount( double amountDeposited)
+        {
+                balance = amountDeposited;
+        }
+        private double balance = 0;
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+        public void Deposit(double amountDeposited)
+        {
+            balance += amountDeposited;
+        }
+
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ BankEncapsulationExercise/BankEncapsulationExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstAccount = new BankAccount(450.00);
+
+            firstAccount.GetBalance();
+            Console.WriteLine($"Your balance is: {firstAccount.Balance}\n");
+
+            var secondAccount = new BankAccount();
+            secondAccount.Deposit(450);
+            Console.WriteLine($"Deposit accepted!\n");
+
+            secondAccount.GetBalance();
+            Console.WriteLine($"Second account balance is: {secondAccount.Balance}\n");
+            
+        }
+    }
+}
